3ffa0203bda6b0a1fc3b181218bb36aa3436132a
Anriu Oliveira <anriu.oliveira12@gmail.com>
2020-12-08 20:59:00 -0300

    Primeira classe, posição

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgamer.Position;
 public class Program {
 
     public static void main(String[] args) {
         
-        System.out.println("Inicio do projeto");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
        
     }
     
