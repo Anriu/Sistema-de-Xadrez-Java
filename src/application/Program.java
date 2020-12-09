f307ba48f780d86dd77c28fb8e002ef95adac3d3
Anriu Oliveira <anriu.oliveira12@gmail.com>
2020-12-08 21:41:20 -0300

    Inicio da implementação das peças e do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,7 @@ public class Program {
 
     public static void main(String[] args) {
         
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        bo
        
     }
     
